51002518de3435e733750ac44f3a1ab9ed561f99
nino <93901798+nino-cast@users.noreply.github.com>
2022-07-03 09:39:39 +0900

    init

---
+++ shared/types/post.ts
@@ -0,0 +1,10 @@
+export type Post = {
+  id: string;
+  title: string;
+  createdAt: number;
+  updatedAt: number;
+  coverUrl: string;
+  authorId: string;
+  likeCount: number;
+  commentCount: number;
+};
---
+++ shared/types/user.ts
@@ -0,0 +1,9 @@
+export type User = {
+  id: string;
+  handleName: string;
+  name: string;
+  photoUrl: string;
+  description: string;
+  followerCount: number;
+  followCount: number;
+};
